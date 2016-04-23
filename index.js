0bb5af9943391467a6e070f15875437883cd0320
Thomas Watson Steen <w@tson.dk>
2016-04-23 02:28:56 +0200

    Fix relative file reading

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 'use strict'
 
+var path = require('path')
 var fs = require('fs')
 
-exports.key = fs.readFileSync('./key.pem')
-exports.cert = fs.readFileSync('./cert.pem')
+exports.key = fs.readFileSync(path.join(__dirname, 'key.pem'))
+exports.cert = fs.readFileSync(path.join(__dirname, 'cert.pem'))
